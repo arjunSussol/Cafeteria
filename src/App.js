93000e0459fe91755f300ebd539599503dfb2cbd
Arjun Sah <arjunsah@sussol.net>
2020-04-22 09:57:37 +0545

    Added Media as component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 
-import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu';
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
           </NavbarBrand>
         </div>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
